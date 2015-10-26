c79cd9f4272a09bccf46d34b3637990ced1a26aa
Sasha <zyrsas@gmail.com>
2015-10-26 17:46:26 +0200

    added DiaryTableViewController

---
+++ MindFactory/DiaryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiaryTableViewController.h
+//  MindFactory
+//
+//  Created by sasha on 26.10.15.
+//  Copyright © 2015 CEIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiaryTableViewController : UITableViewController
+
+@end
